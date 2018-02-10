10837c7b20adeaccc58ad6ff333b0a4e53f8b996
Hoang Le Minh <leminhhoang2509@gmail.com>
2018-02-10 18:41:31 +0200

    first commit

---
+++ app/config.ts
@@ -0,0 +1,9 @@
+export const Config = {
+  Flickr: {
+    CLIENT_ID: "395b9de4969d280e40000dfebd11c0dd",
+    API_URL: "https://api.flickr.com/services/rest/?"
+  },
+  MapBox: {
+    ACCESS_TOKEN: "pk.eyJ1IjoibGVtaW5oaG9hbmcyNTA5IiwiYSI6ImNqZGdqYzdkMzBpZDEydnBoZDAxNDZkemgifQ.3Tx_LwkQm_-stz5ukEnYzQ"
+  }
+}
